bd0f36fb349cf4f58e9f7a22edb6eb75f3f21015
slendyG <nategarza2410@gmail.com>
2024-12-04 12:52:16 -0800

    Remove task creation and print statements; add welcome message to Task Manager

--- taskManager.java
+++ taskManager.java
@@ -1,10 +1,6 @@
 
 public class taskManager {
     public static void main(String[] args) {
-        // Create a new task
-        Task task = new Task("Task 1", "This is the first task", "2021-10-01", "2021-10-02", "High", "To Do");
-
-        // Print the task
-        System.out.println(task);
+        System.out.println("Welcome to your Task Manager!");
     }
 }
