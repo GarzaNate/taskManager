c7027821a76acccbbb208abaa5e3a09782c55957
slendyG <nategarza2410@gmail.com>
2024-12-04 12:40:59 -0800

    Initial commit

---
+++ taskManager.java
@@ -0,0 +1,10 @@
+
+public class taskManager {
+    public static void main(String[] args) {
+        // Create a new task
+        Task task = new Task("Task 1", "This is the first task", "2021-10-01", "2021-10-02", "High", "To Do");
+
+        // Print the task
+        System.out.println(task);
+    }
+}
